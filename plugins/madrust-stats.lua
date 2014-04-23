276c9e7167ae94febf2e353f29a13521a98cbd57
W. Brian Gourlie <bgourlie@gmail.com>
2014-04-23 17:41:43 -0500

    Normalizing models and discovering types.

--- plugins/madrust-stats.lua
+++ plugins/madrust-stats.lua
@@ -7,18 +7,68 @@ PLUGIN.Author = "W. Brian Gourlie"
 
 function PLUGIN:Init()
   print("madrust-stats init")
+
+  -- damage_radiation : damage_radiation: 16
+  -- damage_explosion : damage_explosion: 8
+  -- damage_generic : damage_generic: 1
+  -- damage_melee : damage_melee: 4
+  -- damage_bullet : damage_bullet: 2
+  -- damage_cold : damage_cold: 32
+  typesystem.LoadEnum( Rust.DamageTypeFlags, "DamageType" )
+
+  for x,y in pairs(DamageType) do
+  	print(tostring(x) .. " : " .. tostring(y))
+  end
+
 end
 
 function PLUGIN:OnKilled(takeDamage, damageEvent)
+	local normalizedEvent = self:NormalizeDamageEvent(damageEvent)
 	print(" *** BEGIN DEATH REPORT *** ")
-	print('attacker: ' .. tostring(damageEvent.attacker))
-	print('victim: ' .. tostring(damageEvent.victim))
-	print('sender: ' .. tostring(damageEvent.sender))
-	print('status: ' .. tostring(damageEvent.status))
-	print('damageTypes: ' .. tostring(damageEvent.damageTypes))
-	print('amount: ' .. tostring(damageEvent.amount))
-	print('extraData: ' .. tostring(damageEvent.extraData))
-	print('bodyPart: ' .. tostring(damageEvent.bodyPart))
+	print('attacker: ' .. normalizedEvent.attacker.name)
+	print('victim: ' .. normalizedEvent.victim.name)
+	print('damageTypes: ' .. normalizedEvent.damageTypes)
+	print('extraData: ' .. normalizedEvent.extraData)
+	print('bodyPart: ' .. normalizedEvent.bodyPart)
 	print(" *** END DEATH REPORT *** ")
 end
+
+function PLUGIN:NormalizeDamageEvent(damageEvent)
+	local normalized = 
+	{
+		attacker = self:NormalizeDamageBeing(damageEvent.attacker),
+		victim = self:NormalizeDamageBeing(damageEvent.victim),
+		damageTypes = tostring(damageEvent.damageTypes),
+		extraData = tostring(damageEvent.extraData),
+		bodyPart = tostring(damageEvent.bodyPart)
+	}
+
+	return normalized
+end
+
+function PLUGIN:NormalizeDamageBeing(damageBeing)
+	local name
+	local id
+	local isNpc
+
+	if damageBeing.client then
+		name = damageBeing.client.userName
+		id = damageBeing.client.userID
+		isNpc = false
+	elseif damageBeing.character then
+		name = tostring(damageBeing.character)
+		isNpc = true
+	else
+		name = tostring(damageBeing)
+		isNpc = true
+	end
+
+	return 
+	{
+		name = name, 
+		id = id, 
+		isNpc = isNpc
+	}
+end
+
 
